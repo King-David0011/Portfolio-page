c4abfa8a9cafd98517aa3e2749c871cd4ec0bedd
King-David <kingmadudili07@gmail.com>
2025-01-21 14:01:09 +0100

    Changed the location of different files and deleted some unused backgrounds

--- to-doapp/todo.js
+++ to-doapp/todo.js
@@ -1,4 +1,4 @@
-// Select DOM elements
+
 const taskInput = document.getElementById('taskInput');
 const addTaskButton = document.getElementById('addTaskButton');
 const taskList = document.getElementById('taskList');
