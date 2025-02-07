2beab1f2d95463a9cf9b16ce8dbf6c4bcbb8bac8
King-David <kingmadudili07@gmail.com>
2025-02-07 10:08:52 +0100

    Added a github link that links the portfolio's to their designated code on github. Also, corrected some typos

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ function openmenu(){
     sidemenu.style.right = "0";
 }
 function closemenu(){
-    sidemenu.style.left = "-200px";
+    sidemenu.style.right = "-200px";
 }
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz9XbWxcbdUypao9va8CKCr67h98OxK2f4l7ENQpGlgVioESdHo0elO4YLmkccp1iQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{ 
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
 
 
 
